f49fe7f6a6b0d7aff668ac999cde0deb99384442
Maik van der Gaag <maik@familie-vandergaag.nl>
2021-06-22 17:13:58 +0200

    updated script and pipeline

---
+++ utilities/Get-TemplateSpecVersion.ps1
@@ -0,0 +1,47 @@
+<#
+.SYNOPSIS
+    Script to calculate the next template spec version
+
+.DESCRIPTION
+    Based on the existing version a new version number will be calculated
+    Author: Maik van der Gaag
+    Date: 22-06-2021
+
+
+.PARAMETER TemplateSpecName
+    The name of the template spec
+
+.PARAMETER ResourceGroupName
+    The name of the resource group for the specs
+
+
+.EXAMPLE
+    .\Get-TemplateSpecVersion.ps1 -TemplateSpecName "tempspec" -ResourceGroupName "rg-test"
+#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory = $true)]
+    [String]$TemplateSpecName,
+    [Parameter(Mandatory = $true)]
+    [String]$ResourceGroupName
+)
+
+process{
+    $version = (Get-AzTemplateSpec -Name "$TemplateSpecName" -ResourceGroupName "$ResourceGroupName" -ErrorAction SilentlyContinue).Versions
+    if($version){
+        $version = $version[$version.length -1].Name
+        $version= [string]$version
+        $minor = [int]$version.Split('.')[1]
+        $major =
+         [int]$version.Split('.')[0]
+        $minor = $minor + 1
+        if($minor -eq 9){
+            $major = $major + 1
+        }
+        echo "::set-output name=versionnumber::$major.$minor"
+    }else{
+        echo "::set-output name=versionnumber::0.1"
+    }
+}
